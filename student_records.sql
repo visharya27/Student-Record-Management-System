fb80027f1711eea90bcc1ffae781cf636dd65d0f
Vishwas <vishwasarya2000@gmail.com>
2021-11-09 18:38:23 +0530

    #

--- student_records.sql
+++ student_records.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2021 at 09:12 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
+-- Generation Time: Nov 09, 2021 at 02:00 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -50,8 +49,10 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`name`, `age`, `gender`, `dob`, `email`, `phone`, `fname`, `mname`, `address`, `yoadm`, `rollno`, `dept`, `notes`, `date`) VALUES
-('Vishwas Arya', 21, 'male', '2000-08-27', 'vishwasarya2000@gmail.com', 9413977291, 'Vijay Prakash', 'Sangeeta', 'Plot-30, Sec-12, Hanumangarh Junction', 2019, '195091', 'Computer Science', '', '2021-11-08'),
-('Navya', 19, 'female', '2021-11-05', 'navya@gmail.com', 7828095861, 'XYZ', 'YZA', 'ZAB', 2019, '195096', 'Computer Science', '', '2021-11-09');
+('Gaurav Choudhary', 22, 'male', '1999-01-01', '181064@nith.ac.in', 9000490004, 'Test Name', 'Test Name', 'Test Address', 2018, '181064', 'Civil', '', '2021-11-09'),
+('Vishwas Arya', 21, 'male', '2000-08-21', 'vishwasarya2000@gmail.com', 9413977291, 'Vijay Prakash', 'Sangeeta', 'Plot-30, Sector-12, Hanumangarh, Rajasthan', 2019, '195091', 'Computer Science', '', '2021-11-09'),
+('Gaurav Pandey', 21, 'male', '2000-05-25', '195102@nith.ac.in', 9669233493, 'Umesh Pandey', 'Rajni Pandey', '30, Sai Temple Road, Bilaspur, Chhatisgarh', 2019, '195102', 'Computer Science', 'Library Fee due Rs.60', '2021-11-09'),
+('Gaurav Negi', 20, 'male', '2001-01-01', '195578@nith.ac.in', 9000390003, 'Test Name', 'Test Name', 'Test Address', 2019, '195578', 'Computer Science Dual', 'Hostel Fee Due', '2021-11-09');
 
 -- --------------------------------------------------------
 
@@ -81,6 +82,7 @@ INSERT INTO `users` (`username`, `password`) VALUES
 ALTER TABLE `students`
   ADD UNIQUE KEY `rollno` (`rollno`);
 ALTER TABLE `students` ADD FULLTEXT KEY `name` (`name`,`rollno`);
+ALTER TABLE `students` ADD FULLTEXT KEY `name_2` (`name`);
 
 --
 -- Indexes for table `users`
